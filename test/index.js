dc8df819c28eadc2e297c73ee7997d6fefd31505
cereceres <jesus.edelcereceres@gmail.com>
2016-05-05 16:15:02 -0500

    the test to promiseEmitter are added

--- test/index.js
+++ test/index.js
@@ -3,7 +3,6 @@
 const assert = require('assert')
 const EventEmitter = require('events')
 const Payworks = require('../lib')
-const promiseEmitter = require('../lib/utils/promiseEmitter')
 
 let options = {
   username: 'a7652969',
@@ -15,23 +14,25 @@ let options = {
 let methods = Object.getOwnPropertyNames(Payworks.prototype)
 methods = methods.filter(m => /^(?!constructor)/.test(m))
 
-describe('Banorte Payworks', function() {
-  it('should be a class', function() {
+describe('Banorte Payworks', function () {
+  it('should be a class', function () {
     assert.equal(typeof Payworks, 'function')
-    assert(/^\s*class\s+/.test(Payworks.toString()), 'should be a class')
+    assert(/^\s*class\s+/.test(Payworks.toString()),
+      'should be a class')
   })
 
-  it('should be an instance of Payworks', function() {
+  it('should be an instance of Payworks', function () {
     let payworks = new Payworks()
     assert(payworks instanceof Payworks, 'should be an instance')
   })
 
-  it('should be an instance of EventEmitter', function() {
+  it('should be an instance of EventEmitter', function () {
     let payworks = new Payworks()
-    assert(payworks instanceof EventEmitter, 'should be an instance of EventEmitter')
+    assert(payworks instanceof EventEmitter,
+      'should be an instance of EventEmitter')
   })
 
-  it('should be instanced with options', function() {
+  it('should be instanced with options', function () {
     let payworks = new Payworks(options)
 
     assert.equal(payworks.username, options.username)
@@ -40,7 +41,7 @@ describe('Banorte Payworks', function() {
     assert.equal(payworks.terminal, options.terminal)
   })
 
-  it('should have only this set of public methods', function() {
+  it('should have only this set of public methods', function () {
     let fns = [
       'auth',
       'forceAuth',
@@ -61,44 +62,5 @@ describe('Banorte Payworks', function() {
     for (let fn of fns) {
       assert.equal(typeof Payworks.prototype[fn], 'function')
     }
-  })
-
-  it('The promiseEmitter class have a then and catch function', function(done) {
-    let e = new promiseEmitter(function(res, rej) {
-      console.log('dentro de la promesa');
-      assert(this instanceof promiseEmitter)
-      console.log('antes de random');
-      let random = Math.random()
-      console.log('random=', random);
-      if (random < 0.33) {
-        res(random)
-        return
-      } else if (random < 0.66) {
-        throw random
-        return
-      }
-      rej(random)
-    })
-    e.then(function(res) {
-        console.log('dentro de then');
-        assert(this instanceof promiseEmitter)
-        assert(res < 0.33)
-        done()
-      })
-      .catch(function(error) {
-        console.log('dentro de catch', error);
-        assert(this instanceof promiseEmitter)
-        assert(error > 0.33)
-        done()
-      })
-    this.e = e
-  })
-
-  it('The promiseEmitter class is a event emitter', function(done) {
-    this.e.on('test', function(send) {
-        assert(send === 'hola')
-        done()
-      })
-      .emit('test', 'hola')
   })
 })
---
+++ test/utils/promiseEmitter.js
@@ -0,0 +1,45 @@
+'use strict'
+
+const assert = require('assert')
+const EventEmitter = require('events')
+const PromiseEmitter = require('../../lib/utils/promiseEmitter')
+describe('promiseEmitter', function () {
+  it('The promiseEmitter class have a then and catch function', function (
+    done) {
+    let e = new PromiseEmitter(function (res, rej) {
+      assert(this instanceof PromiseEmitter)
+      let random = Math.random()
+      if (random < 0.33) {
+        res(random)
+        return
+      } else if (random < 0.66) {
+        throw random
+      }
+      rej(random)
+    })
+    e.then(function (res) {
+      assert(this instanceof PromiseEmitter)
+      assert(res < 0.33)
+      return 'tested'
+    })
+      .then(function (resOfthen) {
+        assert(resOfthen === 'tested')
+        done()
+      })
+      .catch(function (error) {
+        assert(this instanceof EventEmitter)
+        assert(this instanceof PromiseEmitter)
+        assert(error > 0.33)
+        done()
+      })
+    this.e = e
+  })
+
+  it('The promiseEmitter class is a event emitter', function (done) {
+    this.e.on('test', function (send) {
+      assert(send === 'hola')
+      done()
+    })
+      .emit('test', 'hola')
+  })
+})
